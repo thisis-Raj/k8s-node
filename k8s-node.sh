0041494904ea6459d64630d8fb2947c8a8042df5
thisis-Raj <106957355+thisis-Raj@users.noreply.github.com>
2022-06-21 15:57:48 +0530

    Create k8s-node.sh

---
+++ k8s-node.sh
@@ -0,0 +1,59 @@
+# This script has to be run as root user.
+
+# set the version of kubernetes you want
+# https://kubernetes.io/releases/
+# apt-cache madison kubeadm | grep 1.22
+# apt-cache madison kubeadm | grep 1.21
+
+# K8S_VERSION=1.22.2-00
+K8S_VERSION=1.21.5-00
+
+# Disable swap
+swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Install docker
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+cat <<EOF | tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+systemctl restart docker
+
+# Install kubeadm
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+
+apt-get install -y kubelet=${K8S_VERSION} kubeadm=${K8S_VERSION} kubectl=${K8S_VERSION}
+apt-mark hold kubelet kubeadm kubectl
+
+docker --version
+kubectl version --client
+kubeadm version
+kubelet --version
+
+# Dependency for nfs-provisioner
+apt-get install -y nfs-common
